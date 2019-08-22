e72557d7dd4393818b69a50628f51dfe1040e638
hiep <daihieptn97@gmail.com>
2019-08-22 23:50:19 +0700

    hello advanced

--- Hello/src/com/servelet/Demo.java
+++ Hello/src/com/servelet/Demo.java
@@ -1,12 +1,17 @@
 package com.servelet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.Session;
 
 /**
  * Servlet implementation class Demo
@@ -52,9 +57,17 @@ public class Demo extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession ss = request.getSession();
+		if ( ss.getAttribute("user") != null) {
+			response.sendRedirect(request.getContextPath() + "/Home");
+			return;
+		}
+
 		String products = "hahahaah";
 		request.setAttribute("products", products);
-		request.getRequestDispatcher("demoHTML.jsp").forward(request, response);
+		RequestDispatcher rq = request.getRequestDispatcher("demoHTML.jsp");
+		rq.forward(request, response);
+
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -65,7 +78,22 @@ public class Demo extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println(request.getParameter("user"));
+
+		String user = request.getParameter("user");
+		String pass = request.getParameter("password");
+
+		if (user.equals("hiep") && pass.equals("1234")) {
+			HttpSession httpSession = request.getSession();
+			httpSession.setAttribute("user", user);
+			httpSession.setAttribute("pass", pass);
+			
+			response.sendRedirect(request.getContextPath() + "/Home");
+		} else {
+			response.getWriter().append("Tên đăng nhập và mật khẩu không đúng");
+		}
+
+		// doGet(request, response);
 	}
 
 	/**
